3b41b6ce0aa071ae1b01a91a044d23c7e3dd7d8b
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-04 18:59:56 -0300

    Add login and sigup

--- client/index.js
+++ client/index.js
@@ -4,6 +4,8 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -20,6 +22,8 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App} />
+        <Route path="/login" component={LoginForm} replace />
+        <Route path="/signup" component={SignupForm} replace />
       </Router>
     </ApolloProvider>
   );
