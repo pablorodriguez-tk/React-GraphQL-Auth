169b5e0bf6a7228d43ad6c9c3b1b7c11ceb3f6b5
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-04 02:33:03 -0300

    Add Header

---
+++ client/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
+
+class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div />;
+    }
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return <div>You are not signed in</div>;
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">{this.renderButtons()}</div>
+      </nav>
+    );
+  }
+}
+
+export default graphql(query)(Header);
--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,10 +1,10 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: 'UserType',
-  fields: { email: { type: GraphQLString } },
+  fields: { id: { type: GraphQLID }, email: { type: GraphQLString } },
 });
 
 module.exports = UserType;
